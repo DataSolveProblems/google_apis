531eeec28fc7b87e28d1e1b4e5f35fbe559b4140
Jie Jenn <44417114+DataSolveProblems@users.noreply.github.com>
2023-01-30 21:39:37 -0600

    Add files via upload

---
+++ google_apis/ytube_analytics.py
@@ -0,0 +1,28 @@
+from .exceptions import AuthException
+from .utility import create_service
+
+class YouTubeAnalytics:
+	API_NAME = 'youtubeAnalytics'
+	API_VERSION = 'v2'
+	SCOPES = ['https://www.googleapis.com/auth/yt-analytics.readonly']
+
+	def __init__(self, client_file):
+		self.client_file = client_file
+		self.service = None
+
+	def init_service(self):
+		self.service = create_service(self.client_file, self.API_NAME, self.API_VERSION, self.SCOPES)
+
+	def run_report(self, start_date, end_date, metrics, dimensions=None):
+		response = self.service.reports().query(
+			ids='channel==MINE',
+			startDate=start_date,
+			endDate=end_date,
+			metrics=','.join(metrics),
+			dimensions=dimensions
+		).execute()
+		columns = [col['name'] for col in response['columnHeaders']]
+		rows = response['rows']
+		return {'columns': columns, 'rows': rows}
+		
+
