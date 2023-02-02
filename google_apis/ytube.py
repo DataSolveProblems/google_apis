bbdca8775c0349f5e901be440bbe4da7d8006697
jiejenn@learndataanalysis.org <jiejenn@learndataanalysis.org>
2023-02-02 10:44:42 -0600

    add playlist functions

--- google_apis/ytube.py
+++ google_apis/ytube.py
@@ -14,3 +14,89 @@ class YouTube:
 	def init_service(self):
 		self.service = create_service(self.client_file, self.API_NAME, self.API_VERSION, self.SCOPES)
 
+
+	def my_playlists(self):
+		"""
+		https://developers.google.com/youtube/v3/docs/playlists/list		
+		"""	
+		playlists = []
+		response = yt.service.playlists().list(
+			part='id,contentDetails,player,snippet,status',
+			mine=True,
+			maxResults=50
+		).execute()
+
+		playlists.extend(response.get('items'))
+		next_page_token = response.get('nextPageToken')
+
+		while next_page_token:
+			response = yt.service.playlists().list(
+				part='id,contentDetails,player,snippet,status',
+				mine=True,
+				maxResults=50,
+				pageToken=next_page_token
+			).execute()			
+			playlists.extend(response.get('items'))
+			next_page_token = response.get('nextPageToken')
+		return playlsits
+
+	def channel_playlists(self, channel_id):
+		"""
+		https://developers.google.com/youtube/v3/docs/playlists/list		
+		"""
+		playlists = []
+		response = yt.service.playlists().list(
+			part='id,contentDetails,player,snippet,status',
+			channelId=channel_id,
+			maxResults=50
+		).execute()
+
+		playlists.extend(response.get('items'))
+		next_page_token = response.get('nextPageToken')
+
+		while next_page_token:
+			response = yt.service.playlists().list(
+				part='id,contentDetails,player,snippet,status',
+				channelId=channel_id,
+				maxResults=50,
+				pageToken=next_page_token
+			).execute()			
+			playlists.extend(response.get('items'))
+			next_page_token = response.get('nextPageToken')
+		return playlsits
+
+    def create_playlist(self, title, description=None, privacy_status='public'):
+        """
+        https://developers.google.com/youtube/v3/docs/playlists#resource
+        """
+        request_body = {
+            'snippet': {
+                'title': title,
+                'description': description,
+            },
+            'status': {
+                'privacyStatus': privacy_status
+            }
+        }
+        response = self.service.playlists().insert(
+            part='snippet,status',
+            body=request_body
+        ).execute()
+        return response
+
+    def update_playlist(self, playlist_id, title, description=None, privacy_status=None):
+        request_body = {
+            'id': playlist_id,
+            'snippet': {
+                'title': title,
+                'description': description
+            },
+            'status': {
+                'privacyStatus': privacy_status
+            }
+        }
+        response = self.service.playlists().update(
+            part='snippet,status',
+            body=request_body
+        ).execute()
+        return response        
